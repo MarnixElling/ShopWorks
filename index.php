4232a14e8fadb6629f413578e76b4bc8c91a07b6
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-02-10 13:25:19 +0100

    Voegt html toe

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Gaaf project haha nee</title>
+        <link type="text/css" href="css/style.css"> 
     </head>
     <body>
     <?php require('views/header.php'); ?>
     <!-- content -->
+    <?php require('views/product.php')?>
     <?php require('views/footer.php'); ?>
     <!-- script links -->
     </body>

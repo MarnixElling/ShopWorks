67886a86dacb57fc366aa667c17458a884901114
Marnix <marnixelling24@gmail.com>
2020-02-11 16:10:55 +0100

    Small patch
    Fixed errors and optimized a few things

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Gaaf project haha nee</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap" rel="stylesheet">
+        <title>ShopWorks</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <script src="js/jquery.js"></script>
-        <script src="js/cards.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/cards.css">
     </head>
     <body>
     <?php require('views/header.php'); ?>
     <!-- content -->
-    <?php require('views/products.php')?>
+    <?php require('views/product.php') ?>
     <?php require('views/footer.php'); ?>
     <!-- script links -->
+    <script src="js/jquery.js"></script>
+    <script src="js/cards.js"></script>
     </body>
 </html>
--- views/product.php
+++ views/product.php
@@ -7,24 +7,24 @@ $data = $api['data'];
 <div class="swipe_cards">
   <div class="demo__content">
     <div class="demo__card-cont">
-<?php
-  foreach($data as $product){
-  echo '
-  <div class="demo__card">
+      <?php
+      foreach ($data as $product) {
+        echo '
+      <div class="demo__card">
         <div class="demo__card__top brown">
-          <div class="demo__card__img" style="background-image : url('. $product["cover"]["media"]["thumbnails"][0]["url"] .')"></div>
-          <p class="demo__card__name">'. $product['media']['thumbnails']['url'] .'</p>
+          <div class="demo__card__img" style="background-image : url(' . $product["cover"]["media"]["thumbnails"][0]["url"] . ')"></div>
+          <p class="demo__card__name">' . $product['media']['thumbnails']['url'] . '</p>
         </div>
         <div class="demo__card__btm">
-          <p class="demo__card__we">Whatever</p>
+          <p class="demo__card__we">Product naam</p>
         </div>
         <div class="demo__card__choice m--reject"></div>
         <div class="demo__card__choice m--like"></div>
         <div class="demo__card__drag"></div>
       </div>
     ';
-  }
-?>
+      }
+      ?>
     </div>
     <p class="demo__tip">Swipe left or right</p>
   </div>

bcf82dfca6ed2342963270f4914c1c2c1a9337b1
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-02-13 19:55:46 +0100

    Allows items to be viewed from the shopping cart

--- views/cart.php
+++ views/cart.php
@@ -1,3 +1,22 @@
+<main>
+    <section>
+        <table>
+            <tr><td>Name</td><td>Price</td><td>Amount</td><td></td>Total</row>
 <?php
-
-?>
+require('config/shopping_cart.php');
+$sql = "SELECT `name`, `price`, count(*) as 'amount' FROM `cart` GROUP BY `name`, `price`";
+$result = $conn->query($sql);
+foreach($result as $row){
+    echo "<tr><td>".$row['name']."</td><td>".$row['price']."</td><td>".$row['amount']."</td><td></td></row>";
+}
+    ?>
+        <?php
+        $sql = "SELECT sum(`price`) as 'total' FROM `cart`";
+        $result = $conn->query($sql);
+        foreach($result as $row){
+            echo "<tr><td></td><td></td><td></td><td>".$row['total']."</td></row>";
+        }
+        ?>
+        </table>
+    </section>
+</main>
--- views/header.php
+++ views/header.php
@@ -1,5 +1,8 @@
 <header>
     <div class="test">
+        <a href="?v=home">Home</a>
+        <a href="?v=products">Products</a>
+        <a href="?v=cart">Cart</a>
     </div>
     <div class='wave'></div>
 </header>

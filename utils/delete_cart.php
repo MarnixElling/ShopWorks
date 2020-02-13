d149ee5b813475c6cafc1c92694ccc4d5255a31d
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-02-13 20:58:30 +0100

    Finishes all backend functionality

--- utils/delete_cart.php
+++ utils/delete_cart.php
@@ -1,10 +1,14 @@
 <?php
 require('../config/shopping_cart.php');
 
-if(isset($_POST['title'])){
+if(isset($_POST['title'])&&isset($_POST['value'])){
     $title = $_POST['title'];
+    $value = $_POST['value'];
 
-    $sql = "DELETE FROM `cart` WHERE `name` = $title LIMIT 1";
-    $conn->query($sql);
+    $sql = "DELETE FROM `cart` WHERE `name` = '$title' AND `price` = '$value' LIMIT 1";
+    var_dump($conn->query($sql));
+    http_response_code(200); 
+}else{  
+    http_response_code(400); 
 }
 ?>
--- views/cart.php
+++ views/cart.php
@@ -1,22 +1,65 @@
 <main>
+    <script>
+        function add_to_cart_raw(name, price){
+        $.ajax(
+            {
+                url: "utils/insert_cart.php", 
+                type : 'POST',
+                data: {title: name, value:price},
+                success: function(result){
+                console.log(result);
+                location.reload();
+            }
+        });
+        }
+
+        function remove_from_cart(name, price){
+        $.ajax(
+            {
+                url: "utils/delete_cart.php", 
+                type : 'POST',
+                data: {title: name, value:price},
+                success: function(result){
+                console.log(result);
+                location.reload();
+            }
+        });
+  }
+    </script>
     <section>
         <table>
-            <tr><td>Name</td><td>Price</td><td>Amount</td><td></td>Total</row>
+            <tr><td>Name</td><td>Price</td><td>Amount</td><td>Total</td></row>
 <?php
 require('config/shopping_cart.php');
 $sql = "SELECT `name`, `price`, count(*) as 'amount' FROM `cart` GROUP BY `name`, `price`";
 $result = $conn->query($sql);
 foreach($result as $row){
-    echo "<tr><td>".$row['name']."</td><td>".$row['price']."</td><td>".$row['amount']."</td><td></td></row>";
+    echo "<tr>
+            <td>".$row['name']."</td>
+            <td>".$row['price']."</td>
+            <td>
+                <div class='add_remove' onclick='add_to_cart_raw(\"".$row['name']."\",\"".$row['price']."\")'>+</div>"
+                .$row['amount'].
+                "<div class='add_remove' onclick='remove_from_cart(\"".$row['name']."\",\"".$row['price']."\")'>-</div>
+            </td>
+            <td></td>
+        </row>";
 }
     ?>
         <?php
         $sql = "SELECT sum(`price`) as 'total' FROM `cart`";
         $result = $conn->query($sql);
         foreach($result as $row){
-            echo "<tr><td></td><td></td><td></td><td>".$row['total']."</td></row>";
+            echo "<tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                <td>".$row['total']."</td></row>";
         }
         ?>
         </table>
+        <div class="buy">
+            <button>Buy</button>
+        </div>
     </section>
 </main>
